00e86ae4d83d3f67607fd7361f83c9b42d5edf4e
Erik Bernhardsson <mail@erikbern.com>
2019-10-06 17:36:56 -0400

    steps default to 3000

--- run_one.py
+++ run_one.py
@@ -11,17 +11,16 @@ if __name__ == '__main__':
     parser.add_argument('--simple', action='store_true')  # draw less, for gif
     parser.add_argument('--method', choices=['classic', 'vline', 'rogue', 'skippable'], required=True)
     parser.add_argument('--output', default='buffet.json')
+    parser.add_argument('--steps', default=3000, type=int)
     # todo: add more arguments
     args = parser.parse_args()
 
     b = Buffet(rate=args.rate, method=args.method)
     pool = multiprocessing.Pool(10)  # only used for drawing, see comment in draw_frame
-    step = 0
-    while True:
+    for step in range(args.steps):
         print('###### step', step)
         data = b.step()
         with open(args.output, 'w') as f:
             json.dump(data, f)
         if args.draw:
             pool.apply(draw_frame, (b, 'frames/%06d.png' % step, args.simple))
-        step += 1

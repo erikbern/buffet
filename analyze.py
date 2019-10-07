c132ef1c99e894438725fcf29aebeee9cdfaa0c3
Erik Bernhardsson <mail@erikbern.com>
2019-10-06 22:22:22 -0400

    analyze stuff

--- analyze.py
+++ analyze.py
@@ -5,13 +5,21 @@ import pandas
 import sys
 
 
+def get_data(fns):
+    for fn in fns:
+        with open(fn, 'r') as f:
+            data = json.load(f)
+        if type(data) == dict:
+            data = data['data']
+        for created, converted, now in data:
+            yield fn, created, converted, now
+
+
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
-        data = json.load(f)
-    df = pandas.DataFrame(data, columns=['created', 'converted', 'now'])
-    df['group'] = 'Everyone'
+    df = pandas.DataFrame(get_data(sys.argv[1:]), columns=['group', 'created', 'converted', 'now'])
     print(df)
     unit, groups, (G, B, T) = convoys.utils.get_arrays(df)
-    convoys.plotting.plot_cohorts(G, B, T)
+    convoys.plotting.plot_cohorts(G, B, T, groups=groups)
+    pyplot.legend()
     convoys.plotting.plot_cohorts(G, B, T, model='gamma', plot_kwargs={'ls': ':'})
     pyplot.show()

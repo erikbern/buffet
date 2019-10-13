62890c55644ccd01bec1914d5e939f73b24587ff
Erik Bernhardsson <mail@erikbern.com>
2019-10-12 23:36:46 -0400

    plotting stuff

--- plot_cohorts.py
+++ plot_cohorts.py
@@ -1,6 +1,7 @@
 import convoys.plotting, convoys.utils
 import json
 from matplotlib import pyplot
+import numpy
 import pandas
 import sys
 
@@ -9,17 +10,23 @@ def get_data(fns):
     for fn in fns:
         with open(fn, 'r') as f:
             data = json.load(f)
-        if type(data) == dict:
-            data = data['data']
-        for created, converted, now in data:
-            yield fn, created, converted, now
+        if data['rate'] < 0.25:
+            continue
+        for created, converted, now in data['data']:
+            yield fn, data['rate'], data['method'], created, converted, now
 
 
 if __name__ == '__main__':
-    df = pandas.DataFrame(get_data(sys.argv[1:]), columns=['group', 'created', 'converted', 'now'])
+    df = pandas.DataFrame(get_data(sys.argv[1:]), columns=['filename', 'rate', 'method', 'created', 'converted', 'now'])
     max_T = max(df['now'])
     df = df[df['created'] >= max_T/3]  # Remove the "burn-in period"
-    unit, groups, (G, B, T) = convoys.utils.get_arrays(df)
-    convoys.plotting.plot_cohorts(G, B, T, groups=groups, ci=0.8)
-    pyplot.legend()
+    unit, groups, (G, B, T) = convoys.utils.get_arrays(df, groups='rate')
+    m = convoys.plotting.plot_cohorts(G, B, T, groups=groups)  #, ci=0.8)
+    ts = numpy.linspace(0, max_T, 1000)
+    for j, group in enumerate(groups):
+        ys = m.cdf(j, ts)
+        t_end, y_end = max((t, y) for t, y in zip(ts, ys) if not numpy.isnan(y))
+        pyplot.text(t_end, 100.*y_end, group, ha='left', va='center')
+
+
     pyplot.show()
--- plot_stats.py
+++ plot_stats.py
@@ -1,20 +1,26 @@
+import convoys.plotting
 import convoys.single, convoys.utils
 import json
 import numpy
 from matplotlib import pyplot
 import pandas
+import scipy.optimize
 import sys
+
+
+SENTINEL = 1e18
 
 
 def get_data(fns, p=80):
     for fn in fns:
         with open(fn, 'r') as f:
             data = json.load(f)
+        if data['rate'] < 0.25:
+            continue
+
         df = pandas.DataFrame(data['data'], columns=['created', 'converted', 'now'])
         max_T = max(df['now'])
-        throughput_1 = (df['created'] >= max_T/3).sum() / (max_T * 2/3)
-        throughput_2 = (df['converted'] >= max_T/3).sum() / (max_T * 2/3)
-        throughput = (throughput_1 * throughput_2)**0.5
+
         df = df[df['created'] >= max_T/3]  # Remove the "burn-in period"
         df['group'] = None
         unit, groups, (G, B, T) = convoys.utils.get_arrays(df)
@@ -22,20 +28,48 @@ def get_data(fns, p=80):
         km.fit(B, T)
         t = numpy.linspace(0, max(T), 1000, endpoint=False)
         y = km.cdf(t)
+        t, y = numpy.append(t, [SENTINEL]), numpy.append(y, [1.0])  # Add sentinel
         average_t = numpy.trapz(t, y)
         percentile_t = numpy.interp(p/100, y, t)
-        # throughput = df['created'].count() / (max_T * 2/3)
 
-        print(data['method'], data['rate'], throughput, percentile_t)
-        yield (data['method'], data['rate'], throughput, percentile_t)
+        print(data['method'], data['rate'], percentile_t)
+        yield (data['method'], data['rate'], percentile_t)
 
 
 if __name__ == '__main__':
-    df = pandas.DataFrame(get_data(sys.argv[1:]), columns=['method', 'rate', 'throughput', 'latency'])
-    for method in df['method'].unique():
-        df_m = df[df['method'] == method]
-        pyplot.scatter(df_m['throughput'].values, df_m['latency'].values, label=method)
+    df = pandas.DataFrame(get_data(sys.argv[1:]), columns=['method', 'rate', 'latency'])
+    max_latency = max(t for t in df['latency'] if t < SENTINEL * 0.01)
+    y_max = max_latency*1.25
+    df['latency_capped'] = df['latency'].apply(lambda z: y_max if z >= y_max else SENTINEL)
 
-    pyplot.yscale('log')
+    colors = pyplot.rcParams['axes.prop_cycle'].by_key()['color']
+    for color, method in zip(colors, df['method'].unique()):
+        df_m = df[df['method'] == method]
+        pyplot.scatter(df_m['rate'].values, df_m['latency'].values, marker='o', label=method, color=color)
+        for x, y in zip(df_m['rate'].values, df_m['latency_capped'].values):
+            delta = 3
+            pyplot.annotate('', xy=(x, y-delta), xytext=(x, y+delta), annotation_clip=False,
+                            arrowprops=dict(color=color, arrowstyle='<-', lw=2))
+
+        xy = [(x, y) for x, y in zip(df_m['rate'].values, df_m['latency'].values)]  # if y < SENTINEL * 0.01]
+        max_x = max(x for x, y in xy if y < SENTINEL * 0.01)
+
+        def loss(params):
+            k0, c0 = params
+            k, c = numpy.exp(k0), max_x + numpy.exp(c0)
+            l = sum((numpy.log(k / (c - x)) - numpy.log(y))**2 for x, y in xy)
+            print(k, c, '->', l)
+            return l
+
+        res = scipy.optimize.minimize(loss, [10, 10])
+        k0, c0 = res.x
+        k, c = numpy.exp(k0), max_x + numpy.exp(c0)
+        print('solution:', k, c)
+        xs = numpy.linspace(0, 2, 1000)
+        pyplot.plot(xs, k / (c - xs), linestyle=':', color=color)
+
+
+    pyplot.grid(True, which='both', axis='both')
     pyplot.legend()
+    pyplot.ylim([0, y_max])
     pyplot.show()

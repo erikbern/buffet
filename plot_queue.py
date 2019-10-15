5d07379c5af355b08a584843ccabede3a992ab6e
Erik Bernhardsson <mail@erikbern.com>
2019-10-14 22:26:09 -0400

    simplification of plots

--- plot_queue.py
+++ plot_queue.py
@@ -6,6 +6,7 @@ import sys
 
 
 def plot_queues(fns):
+    pyplot.style.use('seaborn-whitegrid')
     c = pyplot.cm.get_cmap('viridis')
     colors = c(numpy.linspace(0, 1, len(fns)))
     for color, fn in zip(colors, sorted(fns)):
@@ -19,33 +20,12 @@ def plot_queues(fns):
         pyplot.plot(ts, in_queue, color=color)
         pyplot.text(ts[-1], in_queue[-1], data['rate'], ha='left', va='center', color=color)
 
-
-def fit(ts, in_queue):
-    # try to fit a y = min(kx, m)
-
-    def loss(params, f):
-        loss = numpy.sum((f(params) - in_queue)**2)
-        return loss
-
-    def f1(params):
-        k, m = params
-        return numpy.minimum(k*ts, m)
-
-    def f2(params):
-        k, = params
-        return k*ts
-
-    r1 = scipy.optimize.minimize(loss, (1, 1), args=(f1,))
-    pyplot.plot(ts, f1(r1.x))
-
-    r2 = scipy.optimize.minimize(loss, (1,), args=(f2,))
-    pyplot.plot(ts, f2(r2.x))
-
-    print('ratio:', r1.fun - r2.fun)
+    pyplot.xlabel('Time elapsed (s)')
+    pyplot.ylabel('Number of people in the system')
+    pyplot.legend()
 
 
 if __name__ == '__main__':
     plot_queues(sys.argv[1:])
-    pyplot.legend()
     pyplot.show()
     
--- plot_stats.py
+++ plot_stats.py
@@ -1,85 +1,50 @@
-import convoys.plotting
-import convoys.single, convoys.utils
 import json
 import numpy
 from matplotlib import pyplot
 import pandas
-import scipy.optimize
+import scipy.stats
 import sys
-
-
-SENTINEL = 1e18
 
 
 def get_data(fns, p=80):
     for fn in fns:
         with open(fn, 'r') as f:
             data = json.load(f)
-        if data['rate'] < 0.25:
-            continue
 
-        df = pandas.DataFrame(data['data'], columns=['created', 'converted', 'now'])
-        max_T = max(df['now'])
+        max_t = max(t for _, _, t in data['data'])
+        arrived = sorted(t for t, _, _ in data['data'])
+        left = sorted(t for _, t, _ in data['data'] if t is not None)
+        ts = numpy.linspace(0, max_t, 1000)
+        in_queue = numpy.searchsorted(arrived, ts) - \
+                   numpy.searchsorted(left, ts)
+        in_queue_avg = in_queue[:int(len(in_queue)*2/3)].mean()
 
-        in_queue = df['created'].count() - df['converted'].count()
-        # Use little's law to infer the cycle time
-        littles_latency = in_queue / data['rate']
+        # Use Little's law to infer the cycle time
+        littles_latency = in_queue_avg / data['rate']
 
-        df = df[df['created'] >= max_T/3]  # Remove the "burn-in period"
-        df['group'] = None
-        unit, groups, (G, B, T) = convoys.utils.get_arrays(df)
-        km = convoys.single.KaplanMeier()
-        km.fit(B, T)
-        t = numpy.linspace(0, max(T), 1000, endpoint=False)
-        y = km.cdf(t)
-        t, y = numpy.append(t, [SENTINEL]), numpy.append(y, [1.0])  # Add sentinel
-        # average_t = numpy.trapz(t, y)
-        percentile_latency = numpy.interp(p/100, y, t)
+        # See if the queue is in an equilibrium
+        k1 = sum(1 for t in arrived if t >= max_t/3)
+        k2 = sum(1 for t in left if t >= max_t/3)
+        a, scale = k1 + k2, 1./2
+        rvs = scipy.stats.gamma.rvs(a, 0, scale, size=(10000, 2))
+        probs = numpy.prod(scipy.stats.gamma.pdf(rvs, a, 0, scale), axis=1)
+        prob = numpy.prod(scipy.stats.gamma.pdf([k1, k2], a, 0, scale))
+        p_value = (probs < prob).mean()  # at least as extreme
 
-        print(data['method'], data['rate'], littles_latency, percentile_latency)
-        yield (data['method'], data['rate'], littles_latency, percentile_latency)
+        if p_value >= 0:
+            yield (data['method'], data['rate'], littles_latency)
 
 
 if __name__ == '__main__':
-    df = pandas.DataFrame(get_data(sys.argv[1:]), columns=['method', 'rate', 'latency', 'latency2'])
-    df.plot.scatter('latency', 'latency2')
-    pyplot.ylim([0, 100])
-    pyplot.show()
-
-    max_latency = max(t for t in df['latency'] if t < SENTINEL * 0.01)
-    y_max = max_latency*1.25
-    df['latency_capped'] = df['latency'].apply(lambda z: y_max if z >= y_max else SENTINEL)
+    df = pandas.DataFrame(get_data(sys.argv[1:]), columns=['method', 'rate', 'latency'])
 
     colors = pyplot.rcParams['axes.prop_cycle'].by_key()['color']
     for color, method in zip(colors, df['method'].unique()):
-        df_m = df[df['method'] == method]
+        df_m = df[df['method'] == method]  #  & df['latency'].apply(numpy.isfinite)]
         pyplot.scatter(df_m['rate'].values, df_m['latency'].values, marker='o', label=method, color=color)
-        for x, y in zip(df_m['rate'].values, df_m['latency_capped'].values):
-            delta = 3
-            pyplot.annotate('', xy=(x, y-delta), xytext=(x, y+delta), annotation_clip=False,
-                            arrowprops=dict(color=color, arrowstyle='<-', lw=2))
-
-        continue
-
-        xy = [(x, y) for x, y in zip(df_m['rate'].values, df_m['latency'].values)]  # if y < SENTINEL * 0.01]
-        max_x = max(x for x, y in xy if y < SENTINEL * 0.01)
-
-        def loss(params):
-            k0, c0 = params
-            k, c = numpy.exp(k0), max_x + numpy.exp(c0)
-            l = sum((numpy.log(k / (c - x)) - numpy.log(y))**2 for x, y in xy)
-            print(k, c, '->', l)
-            return l
-
-        res = scipy.optimize.minimize(loss, [10, 10])
-        k0, c0 = res.x
-        k, c = numpy.exp(k0), max_x + numpy.exp(c0)
-        print('solution:', k, c)
-        xs = numpy.linspace(0, 2, 1000)
-        pyplot.plot(xs, k / (c - xs), linestyle=':', color=color)
-
 
     pyplot.grid(True, which='both', axis='both')
+    pyplot.ylabel('Average waiting time (s)')
+    pyplot.xlabel('Arrival rate (1/s)')
     pyplot.legend()
-    pyplot.ylim([0, y_max])
     pyplot.show()
